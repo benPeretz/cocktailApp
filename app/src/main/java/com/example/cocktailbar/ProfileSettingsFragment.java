fc324a22c47efbfa405cc24c7bba6944c87bb43d
liadT22 <liadt22@gmail.com>
2023-01-03 18:00:56 +0200

    added read, and the info is showing in profile settings

--- app/src/main/java/com/example/cocktailbar/ProfileSettingsFragment.java
+++ app/src/main/java/com/example/cocktailbar/ProfileSettingsFragment.java
@@ -7,67 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileSettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileSettingsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileSettingsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileSettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileSettingsFragment newInstance(String param1, String param2) {
-        ProfileSettingsFragment fragment = new ProfileSettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    MainActivity2 mainActivity2;
+    MainActivity mainActivity;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile_settings, container, false);
-
-
-
-
-
-
-
-
+        EditText firstNameText = view.findViewById(R.id.first_name_text_profile_settings);
+        EditText lastNameText = view.findViewById(R.id.last_name_text_profile_settings);
+        EditText phoneNumberText = view.findViewById(R.id.phone_number_text_profile_settings);
+        EditText emailText = view.findViewById(R.id.email_text_profile_settings);
+        mainActivity2 = (MainActivity2) getActivity();
+        mainActivity2.read(firstNameText,lastNameText,phoneNumberText,emailText,"125634");
         return view;
     }
 }
